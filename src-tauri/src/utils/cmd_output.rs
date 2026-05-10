3a74bd7010a27839f08f0cb2fced29ddf5f44822
hj01857655 <1292548381@qq.com>
2026-05-10 14:00:40 +0800

    fix: macOS DMG 打包安装 create-dmg，清理跨平台编译警告

--- src-tauri/src/utils/cmd_output.rs
+++ src-tauri/src/utils/cmd_output.rs
@@ -6,6 +6,7 @@ use encoding_rs::GBK;
 /// 将命令输出字节解码为字符串。
 /// - Windows: 优先按 UTF-8，失败后按 GBK 解码
 /// - 其他平台: UTF-8 lossy
+#[allow(dead_code)]
 pub fn decode_cmd_output(bytes: &[u8]) -> String {
     #[cfg(target_os = "windows")]
     {
